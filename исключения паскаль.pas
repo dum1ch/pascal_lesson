45b0207bc3758346d9d5e975a44e9c34b49e1d49
dum1ch <info@erbolat.kz>
2022-08-23 21:09:49 +0600

    Паскаль

---
+++ исключения паскаль.pas
@@ -0,0 +1,21 @@
+﻿var s:string;
+    f:text;
+    i:integer;
+begin
+  i:=1;
+  while i = 1 do begin
+  try
+    writeln('Укажите путь к файлу');
+    readln(s);
+    assign(f,s);
+    reset(f);
+    readln(f,s);
+    writeln('Длина первой строки файла:',length(s));
+    close(f);
+    i:=0;
+  except
+    writeln('Файл отсутствует или неверный путь');
+  end;
+  end;
+end.    
+    
